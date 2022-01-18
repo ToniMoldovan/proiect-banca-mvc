5229c728a8dbc37a91bbcf61e62740c69de569d2
IancuMilu <iancu.milu@gmail.com>
2022-01-18 18:03:11 +0200

    Creare modele

---
+++ Banca/Models/ContBancar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Data.Entity; //Pt EntityFramework
+using System.ComponentModel.DataAnnotations; //Pentru adnotari
+
+namespace Banca.Models
+{
+    public class ContBancar
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string id_pers { get; set; }
+
+        public string iban { get; set; }
+
+        public int pin { get; set; }
+
+        public double suma { get; set; }
+
+        public string moneda { get; set; }
+    }
+
+    public class ContBancarDbContext : DbContext
+    {
+        public DbSet<ContBancar> ConturiBancare { get; set; }
+    }
+}
